ec2e09245f39343cb72a86ac3435c85069d89657
E-ELMTALAB <arshanelmtalab@gmail.com>
2023-08-06 20:14:56 +0330

    the test file added

---
+++ test.py
@@ -0,0 +1,12 @@
+import re
+
+text = "112.7619 Nm @ 4000 rpm"
+
+pattern = r"(\d+)"
+
+match = re.search(pattern, text)
+if match:
+    number_before_bhp = int(match.group(1))
+    print(number_before_bhp)
+else:
+    print("No match found.")
